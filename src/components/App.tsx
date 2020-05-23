e9cb4dd784bf3cbb85a1ee8afdc098f7ad67037e
Anthony Diké <anthonydike@nyu.edu>
2020-05-23 14:10:35 -0400

    feat: input max -wip

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import RandNum from './RandNum';
+import InputMax from './InputMax';
 
 function App() {
   return (
     <div className="App">
       <header className="App-body">
         <RandNum />
+        <InputMax />
       </header>
     </div>
   );
